dd0ecb4dce2fe02df2d5b386c83789cb097f7136
Joossss <jospa44@gmail.com>
2014-09-28 20:21:55 +0300

    Update kaamerakoosbrushiga(alpha).py
    lwd8cmdi abiga veidi elegantsem ja töötavam kood, värve saab tagasi võtta

--- kaamerakoosbrushiga(alpha).py
+++ kaamerakoosbrushiga(alpha).py
@@ -8,19 +8,12 @@ drawing = False # true if mouse is pressed
 ix,iy = -1,-1
 cap = cv2.VideoCapture(0)
 cv2.namedWindow('image')
-Hmax = 0
-Hmin = 255
-Smax = 0
-Smin = 255
-Vmax = 0
-Vmin = 255
+mins = np.array([255,255,255])
+maxs = np.array([0,0,0])
 #eelnev vaartus
-Hmaxe = 0
-Hmine = 255
-Smaxe = 0
-Smine = 255
-Vmaxe = 0
-Vmine = 255
+minse = []
+maxse = []
+jarjekord = 0
 
 
 cv2.createTrackbar('brush_size','image',1,10, nothing)
@@ -41,49 +34,23 @@ def pixel_max_value(arv1,arv2):
 
 # mouse callback function
 def choose_color(event,x,y,flags,param):
-	global ix,iy,drawing, Hmin, Hmax, Smax, Smin, Vmax, Vmin,brush_size, Hmine, Hmaxe, Smine, Smaxe ,Vmaxe , Vmine
+	global mins, maxs, minse, maxse, jarjekord
 
 	if event == cv2.EVENT_LBUTTONDOWN:
-		drawing = True
-		ix,iy = x,y
-
-	elif event == cv2.EVENT_LBUTTONUP:
-		drawing = False
 		nx = brush_size
 		ny = brush_size
-		for i in range (0,2*brush_size):
-			for p in range(0,2*brush_size):
-		#kontrollib, kas on vaja varvi skaalat muuta
-					if ix-nx+p >= 0 and ix-nx+p <=480 and iy-ny+i >= 0 and iy-ny+i <= 320: # ei laheks yke aarte
-						valik = frame[ix-nx+p,iy-ny+i]
-						if valik[0] < Hmin:
-							Hmine = Hmin
-							Hmin = pixel_min_value(Hmin, valik[0] + vahe)
-						elif valik[0] > Hmax:
-							Hmaxe = Hmax
-							Hmax = pixel_max_value(Hmax, valik[0] + vahe)
-						if valik[1] < Smin:
-							Smine = Smin
-							Smin = pixel_min_value(Smin, valik[1] + vahe)
-						elif valik[1] > Smax:
-							Smaxe = Smax
-							Smax = pixel_max_value(Smax, valik[1] + vahe)
-						if valik[2] < Vmin:
-							Vmine = Vmin
-							Vmin = pixel_min_value(Vmin, valik[2] + vahe)
-						elif valik[2] > Vmax:
-							Vmaxe = Vmax
-							Vmax = pixel_max_value(Vmax, valik[2] + vahe)
+		pixs=hsv[y-ny:y+ny,x-nx:x+nx,:] 
+		minse.append(mins)
+		maxse.append(maxs)
+		mins=np.minimum(mins, pixs.min(0).min(0) - vahe).clip(0, 255).astype('uint8')
+		maxs=np.maximum(maxs, pixs.max(0).max(0) + vahe).clip(0, 255).astype('uint8')
 
 #kustutab viimased maxid minid
 def eelmised_varvid():
-	global Hmin, Hmine, Hmax, Hmaxe, Smin, Smine, Smax, Smaxe, Vmin, Vmine, Vmax, Vmaxe
-	Hmin = Hmine
-	Hmax = Hmaxe
-	Smin = Smine
-	Smax = Smaxe
-	Vmin = Vmine
-	Vmax = Vmaxe
+	global maxs, maxse, mins, minse, jarjekord
+	try:
+		maxs = maxse.pop()
+		mins = minse.pop()
 
 
 cv2.namedWindow('tava')
@@ -95,10 +62,8 @@ while(True):
 	brush_size =cv2.getTrackbarPos('brush_size','image')
 	vahe = cv2.getTrackbarPos('brush_size','image')
 
-	lower = np.array([Hmin,Smin,Vmin])
-	upper = np.array([Hmax,Smax,Vmax])
     # Display the resulting frame
-	mask = cv2.inRange(hsv, lower, upper)
+	mask = cv2.inRange(hsv, mins, maxs)
 
 	cv2.imshow('tava', hsv)
 	cv2.imshow('mask', mask)
