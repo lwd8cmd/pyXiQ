04e3ca516f415c4852cf96a3d0c19e438baf8909
Joossss <jospa44@gmail.com>
2014-09-28 19:24:19 +0300

    Update kaamerakoosbrushiga(alpha).py

--- kaamerakoosbrushiga(alpha).py
+++ kaamerakoosbrushiga(alpha).py
@@ -8,13 +8,19 @@ drawing = False # true if mouse is pressed
 ix,iy = -1,-1
 cap = cv2.VideoCapture(0)
 cv2.namedWindow('image')
-kernel = np.ones((3,3),np.uint8)
 Hmax = 0
 Hmin = 255
 Smax = 0
 Smin = 255
 Vmax = 0
 Vmin = 255
+#eelnev vaartus
+Hmaxe = 0
+Hmine = 255
+Smaxe = 0
+Smine = 255
+Vmaxe = 0
+Vmine = 255
 
 
 cv2.createTrackbar('brush_size','image',1,10, nothing)
@@ -35,7 +41,7 @@ def pixel_max_value(arv1,arv2):
 
 # mouse callback function
 def choose_color(event,x,y,flags,param):
-	global ix,iy,drawing, Hmin, Hmax, Smax, Smin, Vmax, Vmin,brush_size
+	global ix,iy,drawing, Hmin, Hmax, Smax, Smin, Vmax, Vmin,brush_size, Hmine, Hmaxe, Smine, Smaxe ,Vmaxe , Vmine
 
 	if event == cv2.EVENT_LBUTTONDOWN:
 		drawing = True
@@ -48,21 +54,38 @@ def choose_color(event,x,y,flags,param):
 		for i in range (0,2*brush_size):
 			for p in range(0,2*brush_size):
 		#kontrollib, kas on vaja varvi skaalat muuta
-					valik = frame[ix-nx+p,iy-ny+i]
-					if valik[0] < Hmin:
-						Hmin = pixel_min_value(Hmin, valik[0] + vahe)
-					elif valik[0] > Hmax:
-						Hmax = pixel_max_value(Hmax, valik[0] + vahe)
-					if valik[1] < Smin:
-						Smin = pixel_min_value(Smin, valik[1] + vahe)
-					elif valik[1] > Smax:
-						Smax = pixel_max_value(Smax, valik[1] + vahe)
-					if valik[2] < Vmin:
-						Vmin = pixel_min_value(Vmin, valik[2] + vahe)
-					elif valik[2] > Vmax:
-						Vmax = pixel_max_value(Vmax, valik[2] + vahe)
+					if ix-nx+p >= 0 and ix-nx+p <=480 and iy-ny+i >= 0 and iy-ny+i <= 320: # ei laheks yke aarte
+						valik = frame[ix-nx+p,iy-ny+i]
+						if valik[0] < Hmin:
+							Hmine = Hmin
+							Hmin = pixel_min_value(Hmin, valik[0] + vahe)
+						elif valik[0] > Hmax:
+							Hmaxe = Hmax
+							Hmax = pixel_max_value(Hmax, valik[0] + vahe)
+						if valik[1] < Smin:
+							Smine = Smin
+							Smin = pixel_min_value(Smin, valik[1] + vahe)
+						elif valik[1] > Smax:
+							Smaxe = Smax
+							Smax = pixel_max_value(Smax, valik[1] + vahe)
+						if valik[2] < Vmin:
+							Vmine = Vmin
+							Vmin = pixel_min_value(Vmin, valik[2] + vahe)
+						elif valik[2] > Vmax:
+							Vmaxe = Vmax
+							Vmax = pixel_max_value(Vmax, valik[2] + vahe)
 
-		print Hmin, Hmax, Smin, Smax, Vmin, Vmax
+#kustutab viimased maxid minid
+def eelmised_varvid():
+	global Hmin, Hmine, Hmax, Hmaxe, Smin, Smine, Smax, Smaxe, Vmin, Vmine, Vmax, Vmaxe
+	Hmin = Hmine
+	Hmax = Hmaxe
+	Smin = Smine
+	Smax = Smaxe
+	Vmin = Vmine
+	Vmax = Vmaxe
+
+
 cv2.namedWindow('tava')
 cv2.setMouseCallback('tava', choose_color)
 while(True):
@@ -76,13 +99,14 @@ while(True):
 	upper = np.array([Hmax,Smax,Vmax])
     # Display the resulting frame
 	mask = cv2.inRange(hsv, lower, upper)
-	opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN,kernel)
 
 	cv2.imshow('tava', hsv)
-	cv2.imshow('mask', opening)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
+	cv2.imshow('mask', mask)
+	k = cv2.waitKey(1) & 0xFF
+	if k == ord('q'):
 		break
-# mask=((hsv[:,:,0]<10*(hsv[:,:,1]>128))
+	elif k == ord('e'):
+		eelmised_varvid()
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
